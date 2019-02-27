54bca7c6f779657b06e4c97ea1147adc6c5c711a
RainbomSea <1933422976@qq.com>
2019-02-27 15:22:39 +0800

    添加题目内容

--- 001-020/001两数之和.py
+++ 001-020/001两数之和.py
@@ -1,10 +1,16 @@
+"""
+
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+"""
+
 class Solution1:
     """
     暴力破解
     时间复杂度：o(n^2)
     空间复杂度：o(1)
     """
-    
+
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -22,7 +28,7 @@ class Solution2:
     时间复杂度：o(n)
     空间复杂度：o(n)
     """
-    
+
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -35,10 +41,9 @@ class Solution2:
             if(d.get(value) is not None):
                 return [d.get(value), i]
             d[a] = i
-                
+
 
 
 if __name__=="__main__" :
     print(Solution1().twoSum([2, 7, 11, 15], 9))
     print(Solution2().twoSum([2, 7, 11, 15], 9))
-
