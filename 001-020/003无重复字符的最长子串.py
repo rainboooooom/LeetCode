8ef21a2a842d263730ec40fd6a60b46d7bcd5b6b
RainbomSea <1933422976@qq.com>
2019-02-28 16:50:06 +0800

    编写003无重复字符的最长子串.py

---
+++ 001-020/003无重复字符的最长子串.py
@@ -0,0 +1,20 @@
+"""
+
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+"""
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = dict()
+        ans, i = 0, 0 # i用来记录每次字符串的起始位置
+        for j, value in enumerate(s):
+            if d.get(value) is not None:
+                i = max(d[value]+1, i)
+            ans = max(ans, j-i+1)
+            d[value] = j
+        return ans
