d4f082702516f92927a5b2f17ab12c2d6a08c42f
RainbomSea <1933422976@qq.com>
2019-03-05 14:38:27 +0800

    修改代码格式

--- 001-020/007整数反转.py
+++ 001-020/007整数反转.py
@@ -8,9 +8,11 @@ class Solution:
     def reverse(self, x: int) -> int:
         a = abs(x)
         ans = int(str(a)[::-1])
+
         if x < 0:
             ans =  ans * -1
 
-        if ans < (-2**31) or ans > 2**31-1:
+        if ans < -2**31 or ans > 2**31-1:
             return 0
+
         return ans
