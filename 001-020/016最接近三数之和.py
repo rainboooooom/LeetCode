102266dee830d0540299a8756745602dbac66fa7
RainbomSea <1933422976@qq.com>
2019-03-14 13:26:31 +0800

    编写016最接近的三数之和

---
+++ 001-020/016最接近三数之和.py
@@ -0,0 +1,28 @@
+"""
+
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。
+找出 nums 中的三个整数，使得它们的和与 target 最接近。
+返回这三个数的和。假定每组输入只存在唯一答案。
+
+"""
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        ans = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums)-2):
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if abs(s-target) < abs(ans-target):
+                    ans = s
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    return s
+
+        return ans
