465429cd755ed30007582ce10f1dc82f8645a624
RainbomSea <1933422976@qq.com>
2019-04-02 18:18:44 +0800

    编写028实现strStr()

---
+++ 021-040/028实现strStr().py
@@ -0,0 +1,17 @@
+"""
+
+实现 strStr() 函数。
+
+给定一个 haystack 字符串和一个 needle 字符串，
+
+在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
+
+如果不存在，则返回  -1。
+
+"""
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        return haystack.find(needle)
