46103504501bc2927fcaf80e2ce04e8ad6ce7cf3
RainbomSea <1933422976@qq.com>
2019-09-26 15:36:04 +0800

    编写31. 下一个排列

---
+++ leetcode-java/src/main/java/rainbomsea/leetcode/array/NextPermutation.java
@@ -0,0 +1,50 @@
+package rainbomsea.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 31. 下一个排列
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须原地修改，只允许使用额外常数空间。
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        // 找到第一个下标i, 使得nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        // 如果i>=0说明找到了, 没找到就说明这个序列已经是最大的排列
+        if(i >= 0){
+            // 从后往前扫描找到找到第一个下标j使得nums[j] > nums[i], 并交换两个数
+            for(int j = nums.length-1; j > i; j--) {
+                if(nums[j] > nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    break;
+                }
+            }
+        }
+        // 翻转nums(i, nums.length)使其单调增序列
+        int left = i+1;
+        int right = nums.length-1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        new NextPermutation().nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
