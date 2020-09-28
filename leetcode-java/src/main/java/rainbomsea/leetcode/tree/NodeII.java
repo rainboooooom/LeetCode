f95555a0f18d3c8df6d0ee4e75750b1c1d968996
RainbomSea <1933422976@qq.com>
2020-09-28 21:06:14 +0800

    117. 填充每个节点的下一个右侧节点指针 II

---
+++ leetcode-java/src/main/java/rainbomsea/leetcode/tree/NodeII.java
@@ -0,0 +1,21 @@
+package rainbomsea.leetcode.tree;
+
+public class NodeII {
+    public int val;
+    public NodeII left;
+    public NodeII right;
+    public NodeII next;
+
+    public NodeII() {}
+
+    public NodeII(int _val) {
+        val = _val;
+    }
+
+    public NodeII(int _val, NodeII _left, NodeII _right, NodeII _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
