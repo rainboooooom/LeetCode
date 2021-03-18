70f694611518aa5215a34fb5d9b58f39624e6ba7
rainbomsea <rainbomsea@qq.com>
2021-03-18 11:26:01 +0800

    pom添加jdk版本

--- leetcode-java/src/main/java/rainbomsea/leetcode/array/ArrayPartition1.java
+++ leetcode-java/src/main/java/rainbomsea/leetcode/array/ArrayPartition1.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 /**
  * 561. 数组拆分 I
  * 
- * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 
- * 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对,
+ * 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
  * @author RainbomSea
  *
  */
