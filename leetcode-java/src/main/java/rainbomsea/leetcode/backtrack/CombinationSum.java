b0e3f6af9f596146eb707766970f4b683c60938a
RainbomSea <rainbomsea@qq.com>
2021-04-17 15:41:18 +0800

    style: 修改空格

--- leetcode-java/src/main/java/rainbomsea/leetcode/backtrack/CombinationSum.java
+++ leetcode-java/src/main/java/rainbomsea/leetcode/backtrack/CombinationSum.java
@@ -7,11 +7,11 @@ import java.util.List;
 /**
  * 39. 组合总和
  *
- * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
- * candidates 中的数字可以无限制重复被选取。
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * candidates 中的数字可以无限制重复被选取。
  * 说明：
- *  1.所有数字（包括 target）都是正整数。
- *  2.解集不能包含重复的组合。 
+ *  1.所有数字（包括 target）都是正整数。
+ *  2.解集不能包含重复的组合。 
  */
 public class CombinationSum {
 
--- leetcode-java/src/main/java/rainbomsea/leetcode/string/Zigzag_Conversion.java
+++ leetcode-java/src/main/java/rainbomsea/leetcode/string/Zigzag_Conversion.java
@@ -4,8 +4,8 @@ package rainbomsea.leetcode.string;
 /**
  * 6. Z 字形变换
  * 
- * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
- * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
  * L   C   I   R
  * E T O E S I I G
  * E   D   H   N
