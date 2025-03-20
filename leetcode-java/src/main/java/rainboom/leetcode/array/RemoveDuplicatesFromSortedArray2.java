79a03508325654795259e146842445b96b145da7
rainboom <rainbomsea@qq.com>
2025-03-20 10:42:03 +0800

    reAc: 80. 删除有序数组中的重复项 II

---
+++ leetcode-java/src/main/java/rainboom/leetcode/array/RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,18 @@
+package rainboom.leetcode.array;
+
+
+/**
+ * 80. 删除有序数组中的重复项 II
+ *
+ */
+public class RemoveDuplicatesFromSortedArray2 {
+    public int removeDuplicates(int[] nums) {
+		int left = 2;
+		for(int right = 2; right < nums.length; right++) {
+			if(nums[right] != nums[left-2]) {
+				nums[left++] = nums[right];
+			}
+		}
+		return left;
+    }
+}
